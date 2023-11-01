960480c7cac4054edf2fdc71191f072443dffd14
Daniel <dfcb2014@gmail.com>
2023-11-01 00:36:24 +0000

    Save image correctly

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ import axios from "axios";
 import FormData from "form-data";
 import multer from "multer";
 import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public/"));
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+console.log(__dirname);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -36,24 +42,24 @@ app.post("/artistic", upload.single('imageFile'), async (req, res) => {
             `${API_URL}/image/artistic/painting/${style}`,
             formData,
             {
-                responseType: 'stream'
-            },
-            {
                 headers: {
+                    ...formData.getHeaders(),
                     "Apikey": apiKey,
-                }
+                },
+                responseType: 'stream'
             },
         );
         
         const outputFile = response.headers["content-disposition"].match(/filename="([^"]+)"/);
         const fileName = outputFile[1];
-        console.log(JSON.stringify(response.headers));
+        // console.log(JSON.stringify(response.headers));
         // console.log(JSON.stringify(response.headers["content-disposition"]));
         
         // Salvar a imagem no servidor
-        const tempImagePath = `public/images/${fileName}`;
+        const tempImagePath = path.join(__dirname, 'public', 'images', fileName);
         const fileStream = fs.createWriteStream(tempImagePath);
-        res.render("index.ejs", { image: tempImagePath });
+        response.data.pipe(fileStream);
+        res.render("index.ejs", { image: `/images/${fileName}` });
 
     } catch (err) {
         console.error(err);
